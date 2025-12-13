2045eb8cf76de79e57c7fe69ed57ffcdffdee0b2
porto <pedrolportow@gmail.com>
2025-12-13 17:54:14 -0300

    current user route

--- src/main/java/com/example/server/controllers/AuthController.java
+++ src/main/java/com/example/server/controllers/AuthController.java
@@ -1,10 +1,13 @@
 package com.example.server.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -69,5 +72,17 @@ public class AuthController {
     this.repository.save(newUser);
 
     return ResponseEntity.ok(ApiResponse.success(UserResponseDto.fromEntity(newUser)));
+  }
+
+
+  @GetMapping("/me")
+  public ResponseEntity<ApiResponse<UserResponseDto>> me() {
+    var user = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+    if (!(user instanceof UserEntity)) {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApiResponse.error(401, "Usuário não autenticado"));
+    }
+
+    return ResponseEntity.ok(ApiResponse.success(UserResponseDto.fromEntity((UserEntity) user)));
   }
 }
