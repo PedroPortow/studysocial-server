d895a996e784a52952dc0c47accc32f709a7e2a7
porto <pedrolportow@gmail.com>
2026-02-24 21:07:56 -0300

    cascade quando delete um subject

--- src/main/java/com/example/server/entities/SubjectEntity.java
+++ src/main/java/com/example/server/entities/SubjectEntity.java
@@ -1,10 +1,13 @@
 package com.example.server.entities;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -13,6 +16,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -39,6 +43,18 @@ public class SubjectEntity {
 
   @Column(nullable = false, length = 80)
   private String name;
+
+  @OneToMany(mappedBy = "subject", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+  @Builder.Default
+  private List<NoteEntity> notes = new ArrayList<>();
+
+  @OneToMany(mappedBy = "subject", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+  @Builder.Default
+  private List<AssessmentEntity> assessments = new ArrayList<>();
+
+  @OneToMany(mappedBy = "subject", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+  @Builder.Default
+  private List<AbsenceEntity> absences = new ArrayList<>();
 
   @CreationTimestamp
   @Column(name = "created_at", nullable = false, updatable = false)
